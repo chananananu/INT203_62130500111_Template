f02f95621c43621b7146920741f5f7723939e846
Chananan Likitsarawit <chananan.anu@mail.kmutt.ac.th>
2021-01-27 09:56:40 +0700

    [update] index.html, main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// const app = {​​
+//     data() {​​
+//         return {​​
+//             msg: 'Hello, Vue3'
+//         }​​
+//     }​​
+// }​​
+// Vue.createApp(app).mount('#app')
+
+const app = {
+    data() {
+        return {
+            msg: 'Hello, Vue3'
+        }
+    }
+} 
+Vue.createApp(app).mount('#app')
